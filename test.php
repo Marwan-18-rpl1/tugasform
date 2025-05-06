99b91fd28eb1b852d2c10e7fea8b9d9ace668c86
Marwan-18-rpl1 <marwan180609@gmail.com>
2025-05-06 22:54:49 +0930

    forminput

--- test.php
+++ test.php
@@ -2,12 +2,12 @@
 $nama_depan = $_POST['nama_depan'];
 $nama_belakang = $_POST['nama_belakang'];
 $tingkat = $_POST['tingkat'];
-$jenis_kelamin = $_POST['gender'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
 $hobi = isset($_POST['hobi']) ? implode(", ", $_POST['hobi']) : "Tidak ada";
 
 // Menampilkan hasil
 echo "NAMA SAYA ADALAH $nama_depan $nama_belakang. ";
 echo "SAYA SEKOLAH TINGKAT $tingkat. ";
-echo "JENIS KELAMIN SAYA $gender. ";
+echo "JENIS KELAMIN SAYA $jenis_kelamin. ";
 echo "DAN HOBI SAYA $hobi.";
 ?>
