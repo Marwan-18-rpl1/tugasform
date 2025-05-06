050d6ec5cad86e65db30fe5e64acd7d0552cdd78
Marwan-18-rpl1 <marwan180609@gmail.com>
2025-05-06 22:56:36 +0930

    forminput

--- coba.php
+++ coba.php
@@ -15,20 +15,18 @@
         <select name="tingkat">
             <option value="SD">SD</option>
             <option value="SMP">SMP</option>
-            <option value="SMA">SMA</option>
             <option value="SMK">SMK</option>
         </select><br><br>
 
-        <label>Gender:</label><br>
-        <input type="radio" name="gender" value="Male"> Male
-        <input type="radio" name="gender" value="Female"> Female<br><br>
+        <label>Jenis Kelamin:</label><br>
+        <input type="radio" name="jenis_kelamin" value="Laki-laki"> Laki-laki
+        <input type="radio" name="jenis_kelamin" value="Perempuan"> Perempuan<br><br>
 
         <label>Hobi:</label><br>
         <input type="checkbox" name="hobi[]" value="Membaca"> Membaca
-        <input type="checkbox" name="hobi[]" value="Memasak"> Memasak
         <input type="checkbox" name="hobi[]" value="Menulis"> Menulis
         <input type="checkbox" name="hobi[]" value="Olahraga"> Olahraga
-        <input type="checkbox" name="hobi[]" value="Main Game">Main Game<br><br>
+        <input type="checkbox" name="hobi[]" value="Musik"> Musik<br><br>
 
         <input type="submit" value="Kirim">
     </form>
